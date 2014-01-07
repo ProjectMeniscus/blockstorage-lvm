2c410e8bf7c05e423e6874dc12c70f18d80a57a1
Steven Gonzales <stevendgonzales@gmail.com>
2014-01-07 00:10:06 -0600

    update attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default[:rackspace][:cloud][:region] = "ord"
 #Rackspace Cloud Block Storage attributes
 default[:blockstorage_lvm][:no_volumes] = 1
 default[:blockstorage_lvm][:volume_type] = "SATA"
-default[:blockstorage_lvm][:volume_size] = "100"
+default[:blockstorage_lvm][:volume_size] = 100
 
 #LVM attributes
 default[:blockstorage_lvm][:volume_group_name] = "vg00"
